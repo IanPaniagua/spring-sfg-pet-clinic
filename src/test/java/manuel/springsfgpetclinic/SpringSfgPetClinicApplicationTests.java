04b0f840ab71fbaae7f66bcedee51df6754c7000
IanPaniagua <paniagua.ian.de@gmail.com>
2025-02-06 13:11:07 +0100

    starting project

---
+++ src/test/java/manuel/springsfgpetclinic/SpringSfgPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package manuel.springsfgpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSfgPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
